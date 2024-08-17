3fe69525536203f8004e51a333cf49cdd6b0f9fc
joenard12 <joenardprajes123456@gmail.com>
2024-08-17 08:39:25 +0800

    seed post

---
+++ prisma/seed.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const seed = async () => {
+  await prisma.post.createMany({
+    data: [
+      { title: 'title-1', content: 'dummy1' },
+      { title: 'title-2', content: 'dummy2' },
+      { title: 'title-3', content: 'dummy3' },
+      { title: 'title-4', content: 'dummy4' },
+    ],
+  });
+};
+seed(); 
